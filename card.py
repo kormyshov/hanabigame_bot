b7b0668e338f6c9aedb3e0443ef993cbd660464d
Mikhail Kormyshov <kormyshov@gmail.com>
2022-05-30 17:18:45 +0300

    Move tests to new directory

--- card.py
+++ card.py
@@ -33,23 +33,3 @@ class Card:
         if self.number == CardNumbers.FIVE:
             return None
         return Card(self.number + 1, self.color)
-
-
-def test_previous_card_ok():
-    a = Card(CardNumbers.FIVE, CardColors.GREEN)
-    assert a.get_previous_card() == Card(CardNumbers.FOUR, CardColors.GREEN)
-
-
-def test_previous_card_for_one():
-    a = Card(CardNumbers.ONE, CardColors.RAINBOW)
-    assert a.get_previous_card() is None
-
-
-def test_next_card_ok():
-    a = Card(CardNumbers.THREE, CardColors.BLUE)
-    assert a.get_next_card() == Card(CardNumbers.FOUR, CardColors.BLUE)
-
-
-def test_next_card_for_five():
-    a = Card(CardNumbers.FIVE, CardColors.YELLOW)
-    assert a.get_next_card() is None
