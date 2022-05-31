c9bde6713af317d2a063208fe4c2da542a47b6a3
Mikhail Kormyshov <kormyshov@gmail.com>
2022-05-31 18:25:51 +0300

    Refactoring Game

--- game.py
+++ game.py
@@ -1,229 +1,208 @@
 import random
-import logging
+from enum import Enum
+from typing import Literal, Union, List, NamedTuple
 
 import database
-import player
-from sequence import Sequence, previous_card, next_card, init_list
+from player import Player
+from sequence import Sequence
+from card import (
+    Card,
+    CardColors,
+    CardNumbers,
+)
+from exceptions import GameDoesntInit, GameStackIsEmpty, DontExistCard
 
 
-class GameState:
+class GameState(Enum):
     NOT_START = 0
     WAITING_START = 1
+    FINISH = 2
     TURN_PLAYER_ONE = 5
     TURN_PLAYER_TWO = 6
     TURN_PLAYER_THREE = 7
     TURN_PLAYER_FOUR = 8
     TURN_PLAYER_FIVE = 9
-    FINISH = 10
+    TURN_PLAYER_ONE_LAST = 10
+    TURN_PLAYER_TWO_LAST = 11
+    TURN_PLAYER_THREE_LAST = 12
+    TURN_PLAYER_FOUR_LAST = 13
+    TURN_PLAYER_FIVE_LAST = 14
 
 
-class Game:
-    class ConnectResponse:
-        ERROR = 0
-        OK = 1
-        OK_AND_START = 2
+Hint = Union[Literal[0], Literal[1], Literal[2], Literal[3], Literal[4], Literal[5], Literal[6], Literal[7], Literal[8]]
+Live = Union[Literal[0], Literal[1], Literal[2], Literal[3]]
 
-    def __init__(self, game_id):
-        self.id = str(game_id)
-        self.loaded = False
 
-        self.state = GameState.NOT_START
-        self.stack = None
-        self.table = None
-        self.trash = None
-        self.hints = 0
-        self.lives = 0
-        self.players = []
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+    def clear(cls):
+        cls._instances.pop(cls, None)
+
+
+class TableInfo(NamedTuple):
+    table: Sequence
+    hints: Hint
+    lives: Live
+
+
+class ConnectionResult(Enum):
+    OK = 1
+    OK_AND_START = 2
+
+
+class ConnectionResponse(NamedTuple):
+    result: ConnectionResult
+    players: List[Player]
+
+
+def init_list() -> List[Card]:
+    lst: List[Card] = []
+    for color in CardColors:
+        if color == CardColors.RAINBOW:
+            continue
+        for count in range(3):
+            lst.append(Card(CardNumbers.ONE, color))
+        for number in (CardNumbers.TWO, CardNumbers.THREE, CardNumbers.FOUR):
+            for count in range(2):
+                lst.append(Card(number, color))
+        lst.append(Card(CardNumbers.FIVE, color))
+
+    for number in CardNumbers:
+        lst.append(Card(number, CardColors.RAINBOW))
+
+    return lst
+
+
+class Game(metaclass=Singleton):
+    def __init__(self, game_id: str):
+        self.id: str = game_id
+        self.loaded: bool = False
+        self.state: GameState = GameState.NOT_START
+        self.stack: Sequence = Sequence()
+        self.table: Sequence = Sequence()
+        self.trash: Sequence = Sequence()
+        self.hints: Hint = 0
+        self.lives: Live = 0
+        self.players: List[Player] = []
 
     def load(self):
         if not self.loaded:
-            response = database.get_game_info(self.id)
-            if 'Item' in response:
-                self.state = response['Item']['state']
-                self.stack = Sequence.from_str(response['Item'].get('stack', None))
-                self.table = Sequence.from_str(response['Item'].get('table', None))
-                self.trash = Sequence.from_str(response['Item'].get('trash', None))
+            pass
+    #         response = database.get_game_info(self.id)
+    #         if 'Item' in response:
+    #             self.state = response['Item']['state']
+    #             self.stack = Sequence.from_str(response['Item'].get('stack', None))
+    #             self.table = Sequence.from_str(response['Item'].get('table', None))
+    #             self.trash = Sequence.from_str(response['Item'].get('trash', None))
+    #
+    #             for i in range(1, 6):
+    #                 player_id = response['Item'].get('player' + str(i), None)
+    #                 if player_id is not None:
+    #                     self.players.append(player.Player(player_id))
+    #
+    #             self.hints = response['Item'].get('hints', 0)
+    #             self.lives = response['Item'].get('lives', 0)
+    #
+    #         self.loaded = True
+    #
+    # def save(self):
+    #     player_to_id = {('player' + str(k + 1), player.id) for k, player in enumerate(self.players)}
+    #     stack = self.stack.to_str() if self.stack is not None else ''
+    #     table = self.table.to_str() if self.table is not None else ''
+    #     trash = self.trash.to_str() if self.trash is not None else ''
+    #     database.set_game_info(self.id, self.state, stack, table, trash, player_to_id, self.hints, self.lives)
 
-                for i in range(1, 6):
-                    player_id = response['Item'].get('player' + str(i), None)
-                    if player_id is not None:
-                        self.players.append(player.Player(player_id))
+    def get_table_info(self) -> TableInfo:
+        return TableInfo(self.table, self.hints, self.lives)
 
-                self.hints = response['Item'].get('hints', 0)
-                self.lives = response['Item'].get('lives', 0)
+    def get_trash_cards(self) -> Sequence:
+        return self.trash
 
-            self.loaded = True
+    def init_game(self, player: Player) -> None:
+        self.state = GameState.WAITING_START
+        self.connect_player(player)
 
-    def save(self):
-        player_to_id = {('player' + str(k + 1), player.id) for k, player in enumerate(self.players)}
-        stack = self.stack.to_str() if self.stack is not None else ''
-        table = self.table.to_str() if self.table is not None else ''
-        trash = self.trash.to_str() if self.trash is not None else ''
-        database.set_game_info(self.id, self.state, stack, table, trash, player_to_id, self.hints, self.lives)
-
-    def get_table_output(self):
-        self.load()
-        return self.table.to_output() if self.table is not None else '', self.hints, self.lives
-
-    def get_trash_output(self):
-        self.load()
-        return self.trash.to_output() if self.trash is not None else ''
-
-    def finish(self):
-        self.load()
+    def finish(self) -> None:
         for player in self.players:
             player.finish()
 
         database.finish_game(self.id)
 
-    def to_waiting_start(self, player):
-        self.players.append(player)
-        self.state = GameState.WAITING_START
-        self.save()
-
-    def connect_player(self, player):
-        logger = logging.getLogger('hanabigame.game.connect_player')
-        logger.info('start')
-        self.load()
-        logger.info('load')
+    def connect_player(self, player: Player) -> ConnectionResponse:
         if self.state != GameState.WAITING_START:
-            logger.info('return error')
-            return Game.ConnectResponse.ERROR, []
+            raise GameDoesntInit
 
         self.players.append(player)
         player.connect_to_game(self)
-        self.save()
-        logger.info('saved game')
-        return (Game.ConnectResponse.OK if len(self.players) < 5 else Game.ConnectResponse.OK_AND_START), self.players
 
-    def start(self):
-        print('in Game.start')
-        self.load()
-        print('loaded')
+        return ConnectionResponse(
+            ConnectionResult.OK if len(self.players) < 5 else ConnectionResult.OK_AND_START,
+            self.players,
+        )
 
+    def start(self) -> None:
         lst = init_list()
         random.shuffle(lst)
         cnt = 5 if len(self.players) < 4 else 4
-        print('generate stack')
 
         for i, p in enumerate(self.players):
             p.start_game(Sequence(lst[i * cnt:(i + 1) * cnt]))
-        print('players started game')
 
         self.stack = Sequence(lst[cnt * len(self.players):])
-        self.table = Sequence([])
-        self.trash = Sequence([])
         self.state = GameState.TURN_PLAYER_ONE
 
         self.hints = 8
         self.lives = 3
 
-        self.save()
-        print('save game')
+    def take_card(self) -> Card:
+        try:
+            return self.stack.pop()
+        except IndexError:
+            self.state += 5
+            raise GameStackIsEmpty
 
-        return self.players
+    def add_to_trash(self, card: Card) -> None:
+        self.trash.append(card)
 
-    def take_card(self):
-        logger = logging.getLogger('hanabigame.game.take_card')
-        logger.info('start')
-        self.load()
-        logger.info('loaded')
-        taked_card = self.stack.pop()
-        logger.info('get taked card')
-        self.save()
-        logger.info('save game')
-        return taked_card
+    def add_to_table(self, card: Card) -> bool:
+        try:
+            previous_card = card.get_previous_card()
+            if previous_card not in self.table:
+                self.add_to_trash(card)
+                self.lives -= 1
+                return False
+            index = self.table.index(previous_card)
+            self.table.pop(index)
+        except DontExistCard:
+            pass
+        self.table.append(card)
 
-    def add_to_trash(self, card_number):
-        logger = logging.getLogger('hanabigame.game.add_to_trash')
-        logger.info('start')
-        self.load()
-        logger.info('loaded')
-        if self.trash is not None:
-            self.trash.append(card_number)
-        else:
-            self.trash = Sequence([card_number])
-        logger.info('add card to trash')
-        self.save()
-        logger.info('save game')
+        try:
+            card.get_next_card()
+        except DontExistCard:
+            self.hints = min(self.hints + 1, 8)
+        return True
 
-    def add_to_table(self, card_number):
-        logger = logging.getLogger('hanabigame.game.add_to_table')
-        logger.info('start')
-        self.load()
-        logger.info('loaded')
-        prev_card_number = previous_card(card_number)
-        logger.info('get prev card number')
-        if prev_card_number is None or (self.table is not None and self.table.contains(prev_card_number)):
-            logger.info('success branch')
-            if prev_card_number is None:
-                logger.info('first card')
-                if self.table is not None:
-                    self.table.append(card_number)
-                else:
-                    self.table = Sequence([card_number])
-            else:
-                logger.info('not first card')
-                index = self.table.index(prev_card_number)
-                self.table.pop(index)
-                self.table.append(card_number)
-
-            if next_card(card_number) is None:
-                self.hints = min(self.hints + 1, 8)
-            self.save()
-            return True
-        else:
-            logger.info('fail branch')
-            self.add_to_trash(card_number)
-            logger.info('add to trash')
-            self.lives -= 1
-            logger.info('decrease lives')
-            self.save()
-            return False
-
-    def hint_color(self, recipient_number, color):
-        logger = logging.getLogger('hanabigame.game.hint_color')
-        logger.info('start')
-        self.load()
-        logger.info('loaded')
-        print(recipient_number)
-        print(self.players)
+    def hint_color(self, recipient_number: int, color: CardColors) -> List[int]:
         recipient = self.players[recipient_number]
-        print(recipient.get_name())
-        print(recipient)
-        print(recipient.id)
         card_numbers = recipient.get_card_numbers_with_color(color)
-        logger.info('get card numbers ' + ','.join(map(str, card_numbers)))
         self.hints -= 1
-        logger.info('decrease hints')
         return card_numbers
 
-    def hint_value(self, recipient_number, value):
-        logger = logging.getLogger('hanabigame.game.hint_value')
-        logger.info('start')
-        self.load()
-        logger.info('loaded')
-        print(recipient_number)
-        print(self.players)
+    def hint_value(self, recipient_number: int, value: CardNumbers) -> List[int]:
         recipient = self.players[recipient_number]
-        print(recipient.get_name())
-        print(recipient)
-        print(recipient.id)
         card_numbers = recipient.get_card_numbers_with_value(value)
-        logger.info('get card numbers ' + ','.join(map(str, card_numbers)))
         self.hints -= 1
-        logger.info('decrease hints')
         return card_numbers
 
-    def next_turn(self):
-        logger = logging.getLogger('hanabigame.game.next_turn')
-        logger.info('start')
-        self.load()
-        logger.info('loaded')
+    def next_turn(self) -> int:
         self.state += 1
-        if self.state - len(self.players) == 5:
+        if self.state - len(self.players) == GameState.TURN_PLAYER_ONE:
             self.state = GameState.TURN_PLAYER_ONE
-        logger.info('set new state')
-        self.save()
-        logger.info('save game')
-        return self.state - 5
+        return self.state - GameState.TURN_PLAYER_ONE
---
+++ tests/game.py
@@ -0,0 +1,89 @@
+from typing import Literal
+
+import pytest
+from sequence import Sequence
+from card import (
+    Card,
+    CardNumbers,
+    CardColors,
+)
+from game import (
+    init_list,
+    Game,
+    TableInfo,
+)
+
+
+def test_init_list():
+    lst = init_list()
+    assert len(lst) == 55
+
+    s = Sequence(lst)
+    assert len(s.get_card_numbers(lambda c: c.number == CardNumbers.ONE)) == 16
+    assert len(s.get_card_numbers(lambda c: c.number == CardNumbers.TWO)) == 11
+    assert len(s.get_card_numbers(lambda c: c.number == CardNumbers.THREE)) == 11
+    assert len(s.get_card_numbers(lambda c: c.number == CardNumbers.FOUR)) == 11
+    assert len(s.get_card_numbers(lambda c: c.number == CardNumbers.FIVE)) == 6
+
+    assert len(s.get_card_numbers(lambda c: c.color == CardColors.BLUE)) == 10
+    assert len(s.get_card_numbers(lambda c: c.color == CardColors.RED)) == 10
+    assert len(s.get_card_numbers(lambda c: c.color == CardColors.GREEN)) == 10
+    assert len(s.get_card_numbers(lambda c: c.color == CardColors.YELLOW)) == 10
+    assert len(s.get_card_numbers(lambda c: c.color == CardColors.WHITE)) == 10
+    assert len(s.get_card_numbers(lambda c: c.color == CardColors.RAINBOW)) == 5
+
+
+def test_get_table_info_empty():
+    g = Game('1')
+    assert g.get_table_info() == TableInfo(Sequence(), 0, 0)
+
+
+def test_get_table_info_started():
+    g = Game('1')
+    g.start()
+    assert g.get_table_info() == TableInfo(Sequence(), 8, 3)
+    Game.clear()
+
+
+def test_get_table_info_add_to_table():
+    g = Game('1')
+    g.start()
+    a = Card(CardNumbers.ONE, CardColors.RAINBOW)
+    g.add_to_table(a)
+    assert g.get_table_info() == TableInfo(Sequence([a]), 8, 3)
+    Game.clear()
+
+
+def test_get_table_info_add_to_table_twice():
+    g = Game('1')
+    g.start()
+    a = Card(CardNumbers.ONE, CardColors.RAINBOW)
+    b = Card(CardNumbers.TWO, CardColors.RAINBOW)
+    g.add_to_table(a)
+    g.add_to_table(b)
+    assert g.get_table_info() == TableInfo(Sequence([b]), 8, 3)
+    Game.clear()
+
+
+def test_get_trash_cards_empty():
+    g = Game('1')
+    assert g.get_trash_cards() == Sequence()
+
+
+def test_get_trash_cards():
+    g = Game('1')
+    a = Card(CardNumbers.ONE, CardColors.RAINBOW)
+    g.add_to_trash(a)
+    assert g.get_trash_cards() == Sequence([a])
+    Game.clear()
+
+
+def test_get_trash_cards_many():
+    g = Game('1')
+    a = Card(CardNumbers.ONE, CardColors.RAINBOW)
+    b = Card(CardNumbers.TWO, CardColors.RAINBOW)
+    g.add_to_trash(a)
+    g.add_to_trash(b)
+    g.add_to_trash(a)
+    assert g.get_trash_cards() == Sequence([a, b, a])
+    Game.clear()
