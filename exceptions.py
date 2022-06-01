e98eb471321731d9b1b191be2e3a9726fa9217d0
Mikhail Kormyshov <kormyshov@gmail.com>
2022-06-01 15:36:07 +0300

    Add exceptions for db reading

--- exceptions.py
+++ exceptions.py
@@ -6,9 +6,21 @@ class CardIsNotInSequence(Exception):
     pass
 
 
+class GameDoesntExistInDB(Exception):
+    pass
+
+
 class GameDoesntInit(Exception):
     pass
 
 
 class GameStackIsEmpty(Exception):
+    pass
+
+
+class PlayerDoesntExistInDB(Exception):
+    pass
+
+
+class UnexpectedPlayerState(Exception):
     pass
