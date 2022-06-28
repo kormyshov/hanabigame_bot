795604f674d108085d40331d89b6cdca0e190bcd
Mikhail Kormyshov <kormyshov@gmail.com>
2022-06-28 18:03:31 +0300

    Test logging

--- index.py
+++ index.py
@@ -5,6 +5,10 @@ from main import bot
 
 def handler(event, _):
     logging.getLogger().setLevel(logging.INFO)
+    root_handler = logging.getLogger().handlers[0]
+    root_handler.setFormatter(logging.Formatter(
+        '[%(levelname)s]\t%(name)s\t%(request_id)s\t%(message)s\n'
+    ))
     logging.info('Program started')
 
     message = telebot.types.Update.de_json(event['body'])
