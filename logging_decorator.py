7bfa27f29f58817c8aa79e4d000fcf1a2da06be0
Mikhail Kormyshov <kormyshov@gmail.com>
2022-06-29 11:23:37 +0300

    Fix logging of exception

--- logging_decorator.py
+++ logging_decorator.py
@@ -3,11 +3,11 @@ import logging
 
 def logger(func):
     def wrapper(self, *argv, **kwargv):
-        logging.info('%s %s %s', func.__name__, argv, kwargv)
+        logging.info('%s %s %s', func.__name__, ' '.join(map(str, argv)), kwargv)
         try:
             ret = func(self, *argv, **kwargv)
         except Exception as e:
-            logging.error(e.__doc__)
+            logging.error(e.__class__.__name__)
             raise e
         logging.info('%s return %s', func.__name__, ret)
         return ret
